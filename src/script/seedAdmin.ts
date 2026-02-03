8a5d8e9103cdd0b5408cf831be35ddc655936f0f
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-03 13:22:08 +0600

    seedAdmin added

---
+++ src/script/seedAdmin.ts
@@ -0,0 +1,57 @@
+import config from "../config";
+import { Role } from "../constants/enum";
+import { prisma } from "../lib/prisma";
+
+
+async function seedAdmin(){
+  try {
+    console.log('*** Admin Seeding Seeding... ***');
+    const adminData = {
+        name:'Admin',
+        email: config.admin.email!,
+        password: config.admin.password!,
+        role: Role.ADMIN,
+    }
+
+    console.log('*** Check Admin Exist or Not... ***');
+
+    //? check if the user exist or not
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email: adminData.email,
+        },
+    })
+    if(existingUser){
+        throw new Error('User already exist')
+    }
+
+    console.log('*** Create Admin... ****');
+    const signupAdmin = await fetch(
+        `${config.better_auth.url}/api/auth/sign-up/email`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Origin:'http://localhost:3000',
+            },
+            body:JSON.stringify(adminData)
+        }
+    );
+
+    if(signupAdmin.ok){
+        console.log('*** Admin Created... ***');
+        await prisma.user.update({
+            where:{
+                email:adminData.email,
+            },
+            data:{
+                 emailVerified:true,
+            }
+        })
+        console.log('*** Email Verification Status Updated... ***')
+    }
+    console.log('*** Admin Seeding Completed... ****')
+  } catch (error:any) {
+    console.log('Error', error)
+  }
+}
+seedAdmin()
