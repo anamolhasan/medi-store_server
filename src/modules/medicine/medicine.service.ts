d2af9db19ed98022facaa4f3511467a767ea7814
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-04 12:30:44 +0600

    create medicine added:

--- src/modules/medicine/medicine.service.ts
+++ src/modules/medicine/medicine.service.ts
@@ -1,7 +1,28 @@
+import { Medicine } from "../../../generated/prisma/client"
+import { prisma } from "../../lib/prisma";
+import { AppError } from "../../middleware/appError";
 
 
-const createMedicine = async () => {
+const createMedicine = async (payload:Medicine) => {
+  const {
+    name,
+    description,
+    price,
+    stock,
+    manufacturer,
+    imageUrl,
+    categoryId,
+    sellerId,
+  } = payload;
 
+   // 1️⃣ Validate seller
+   const seller = await prisma.user.findUnique({
+    where: {id:sellerId}
+   })
+
+   if(!seller){
+    throw new AppError('Seller not found', 404)
+   }
 }
 
 
